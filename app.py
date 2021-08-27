f0e76e906f2b8727dfa4683e37c328f4ed62d9b8
trudypainter <tpainter@mit.edu>
2021-08-27 15:46:59 -0400

    basic auth works

--- app.py
+++ app.py
@@ -1,7 +1,84 @@
-from flask import Flask
+from flask import Flask, make_response, session, redirect, request, jsonify, render_template
+import requests
+import os
+import random as rand
+import string
+import pitchfork
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+def makeGetRequest(session, url, params={}):
+	headers = {"Authorization": "Bearer {}".format(session['token'])}
+	response = requests.get(url, headers=headers, params=params)
+
+	if response.status_code == 200:
+		return response.json()
+
+	return None
+
+def getUserInformation(session):
+	url = 'https://api.spotify.com/v1/me'
+	payload = makeGetRequest(session, url)
+
+	if payload == None:
+		return None
+
+	return payload
+
+
+def getToken(code):
+	token_url = 'https://accounts.spotify.com/api/token'
+	authorization = app.config['AUTHORIZATION']
+	redirect_uri = app.config['REDIRECT_URI']
+
+	headers = {'Authorization': authorization, 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}
+	body = {'code': code, 'redirect_uri': redirect_uri, 'grant_type': 'authorization_code'}
+	post_response = requests.post(token_url, headers=headers, data=body)
+
+	print("THE SECOND CALL RESPONSE WAS: ", post_response.status_code)
+
+	# 200 code indicates access token was properly granted
+	if post_response.status_code == 200:
+		json = post_response.json()
+		return json['access_token'], json['refresh_token'], json['expires_in']
+	else:
+		return None
+
+@app.route('/')
+def authorize():
+	client_id = app.config['CLIENT_ID']
+	client_secret = app.config['CLIENT_SECRET']
+	redirect_uri = app.config['REDIRECT_URI']
+	scope = app.config['SCOPE']
+
+	# redirect user to Spotify authorization page
+	authorize_url = 'https://accounts.spotify.com/en/authorize?'
+	parameters = 'response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=' + scope #+'&state=' + state_key
+	response = make_response(redirect(authorize_url + parameters))
+
+	return response
+
+@app.route('/callback')
+def callback():
+	session = dict()
+	# check spotify response
+	if request.args.get('error'):
+		return "error in spotify auth"
+	else:
+		code = request.args.get('code')
+		print("THIS IS THE CODE: ", code)
+
+		# get access token to make requests on behalf of the user
+		payload = getToken(code)
+		if payload != None:
+			session['token'] = payload[0]
+			session['refresh_token'] = payload[1]
+		else:
+			return "error getting access token"
+
+	current_user = getUserInformation(session)
+	session['user_id'] = current_user['id']
+
+	return current_user['id']
