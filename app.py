36441aee7ad91bb392462372f67823f24190fa7b
trudypainter <tpainter@mit.edu>
2021-09-02 15:50:52 -0400

    removed env config

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import string
 import pitchfork_api
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
 
 def getPrettyDuration(duration):
 	if duration == "short_term":
